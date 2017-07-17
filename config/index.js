7db4361ab16f5d82f3d34c7b099f475a0d79623c
u3u <quq@quq.cat>
2017-07-17 16:23:23 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 3000,
     autoOpenBrowser: true,
     hotModuleReload: true,
     assetsSubDirectory: 'static',
