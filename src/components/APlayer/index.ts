392ae9e13938effab3ae131fe30e69c536ea9e74
u3u <quq@quq.cat>
2017-07-17 16:21:04 +0800

    export all

--- src/components/APlayer/index.ts
+++ src/components/APlayer/index.ts
@@ -1,4 +1,4 @@
-import { APlayer } from './APlayer'
+import { APlayer, vPicture, vButton, Container, Controller, Info, Lyric, ProgressBar, TimeBar, Volume, List, Item } from './APlayer'
 
-export { APlayer }
+export { APlayer, vPicture, vButton, Container, Controller, Info, Lyric, ProgressBar, TimeBar, Volume, List, Item }
 export default APlayer
