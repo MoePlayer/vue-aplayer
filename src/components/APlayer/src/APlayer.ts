a701eb6e621ad16d6ab216e3e31a518c91ba6395
u3u <quq@quq.cat>
2017-07-18 16:41:56 +0800

    绑定已播放的进度和缓冲的进度数据

--- src/components/APlayer/src/APlayer.ts
+++ src/components/APlayer/src/APlayer.ts
@@ -21,6 +21,19 @@ import store from 'store'
 import { State } from 'store/state'
 import { SET_MUSIC, SET_THEME, SYNC_MEDIA } from 'store/types'
 
+enum ReadyState {
+  /** 没有关于音频是否就绪的信息 */
+  HAVE_NOTHING = '0',
+  /** 关于音频就绪的元数据 */
+  HAVE_METADATA = '1',
+  /** 关于当前播放位置的数据是可用的，但没有足够的数据来播放下一帧/毫秒 */
+  HAVE_CURRENT_DATA = '2',
+  /** 当前及至少下一帧的数据是可用的 */
+  HAVE_FUTURE_DATA = '3',
+  /** 可用数据足以开始播放 */
+  HAVE_ENOUGH_DATA = '4'
+}
+
 @WithRender
 @Component({ components: { vPicture, vButton, Container, Controller, Info, Lyric, ProgressBar, TimeBar, Volume, List, Item }, store })
 export default class APlayer extends Vue {
@@ -78,6 +91,17 @@ export default class APlayer extends Vue {
   /** 同步 Audio 对象属性以更新视图 */
   @Mutation(SYNC_MEDIA)
   private syncMedia: (audio: HTMLAudioElement) => void
+  /** 已播放的进度比例 */
+  public get played (): number {
+    return this.media.currentTime / this.media.duration
+  }
+  /** 已缓冲的进度比例 */
+  public get loaded (): number {
+    if (this.media.readyState >= ReadyState.HAVE_METADATA) {
+      return this.media.buffered.end(this.media.buffered.length - 1) / this.media.duration
+    }
+    return 0
+  }
 
   private beforeCreate () {
     console.log('\n %c APlayer 1.6.1 %c http://aplayer.js.org \n\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;')
