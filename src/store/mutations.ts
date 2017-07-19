12c020c1282abc979f9fb66d14e4e99d0ab14ba0
u3u <quq@quq.cat>
2017-07-19 16:48:28 +0800

    如果 music 没有指定 id 则通过索引自动生成

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -13,7 +13,12 @@ import {
 } from './types'
 
 export const mutations = {
-  [ADD_MUSICS]: (state: State, musics: Array<APlayer.Music>) => state.list = state.list.concat(musics),
+  [ADD_MUSICS]: (state: State, musics: Array<APlayer.Music>) => {
+    state.list = state.list.concat(musics).map((music, index) => {
+      if (!music.id) music.id = index // 如果 music 没有指定 id 则通过索引自动生成
+      return music
+    })
+  },
   [SET_MUSIC]: (state: State, music: APlayer.Music) => state.music = music,
   [SET_THEME]: (state: State, theme: string) => state.theme = theme,
   [SET_SPEED]: (state: State, speed: number) => state.audio.playbackRate = speed,
