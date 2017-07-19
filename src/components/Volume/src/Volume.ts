301071db2482f9cc1b67a6c0b3eeb7fe5ba66a3b
u3u <quq@quq.cat>
2017-07-19 12:49:53 +0800

    完成音量控制

--- src/components/Volume/src/Volume.ts
+++ src/components/Volume/src/Volume.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import WithRender from './Volume.html?style=./Volume.scss'
 
-import { Prop } from 'vue-property-decorator'
+import { Prop, Watch } from 'vue-property-decorator'
 import { Getter } from 'vuex-class'
 
 import { Button as vButton } from 'components/Button'
@@ -10,8 +10,38 @@ import { Button as vButton } from 'components/Button'
 @WithRender
 @Component({ components: { vButton } })
 export class Volume extends Vue {
-  @Prop({ type: Number, default: 0.8, required: false })
+
+  @Prop({ type: Number, default: 1, required: false })
   public readonly value: number
   @Getter('theme')
   public readonly theme: string
+
+  private currentValue = 1
+  private status = 'down'
+
+  private created (): void {
+    this.valueChange()
+  }
+
+  @Watch('value')
+  private valueChange (): void {
+    this.currentValue = this.value
+    if (this.value > 0.9) this.status = 'up'
+    else if (this.value <= 0) this.status = 'off'
+    else this.status = 'down'
+  }
+
+  private changeHandler (): void {
+    const evt = event as MouseEvent
+    const target = this.$refs.bar as HTMLElement
+    const targetTop = target.getBoundingClientRect().bottom
+    if (targetTop <= 0) return // 音量控制面板已隐藏
+    const offsetTop = Math.round(targetTop - evt.clientY)
+    let percent = offsetTop / target.offsetHeight
+    if (percent > 1) percent = 1
+    if (percent < 0) percent = 0
+    this.currentValue = percent
+    this.$emit('change', percent)
+  }
+
 }
