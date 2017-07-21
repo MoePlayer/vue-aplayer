64c5a9bdff2842bb8983369884dd5740cb935b91
u3u <quq@quq.cat>
2017-07-21 15:31:43 +0800

    优化歌词面板显示
    1. 不要显示空行
    2. 去掉末尾的空行

--- src/components/Lyric/src/Lyric.ts
+++ src/components/Lyric/src/Lyric.ts
@@ -40,6 +40,12 @@ export class Lyric extends Vue {
     const { time } = this.current || { time: 0 }
     const lrcElements = this.$refs.lrc as Array<HTMLElement> || []
     const currentElement = lrcElements.find(x => Number.parseInt(x.dataset.time) === time)
+    const currentIndex = currentElement ? Number.parseInt(currentElement.dataset.index) : 0
+    const isLastElement = currentIndex === lrcElements.length - 1
+    // 如果 currentElement 是最后一个元素
+    // 则返回 currentElement 上一个元素的 offsetTop
+    // 去掉末尾的空行 优化歌词面板显示
+    if (isLastElement) return lrcElements[currentIndex - 1].offsetTop * -1
     return (currentElement ? currentElement.offsetTop : 0) * -1
   }
 
@@ -76,6 +82,7 @@ export class Lyric extends Vue {
       const milliseconds = Number.parseInt(match[3])
       const time = minutes * 60 * 1000 + seconds * 1000 + milliseconds
       const text = (match[4] as string).replace(regTime, '')
+      if (!text) return // 优化：不要显示空行
       this.LRC.push({ time, text })
       matchAll(match[4]) // 递归匹配多个时间标签
     }
