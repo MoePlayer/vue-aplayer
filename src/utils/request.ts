6fcd35009a6758d638e2391118ac9855cebbb1de
u3u <quq@quq.cat>
2017-07-27 17:49:05 +0800

    接口兼容字符串格式

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,6 +4,7 @@ const { baseURL } = Config
 
 export async function request<T> (url: string, params?: T): Promise<AxiosResponse> {
   const response = await axios({ baseURL, url, params })
+  response.data = typeof response.data === 'string' ? JSON.parse(response.data) : response.data
   response.data.success = response.data.code === 200
   return response
 }
