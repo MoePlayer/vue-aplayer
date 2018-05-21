75b84fd2a976238727b2a06bd08bd5498ca88e32
u3u <qwq@qwq.cat>
2018-05-21 12:37:49 +0800

    feat: initialize the sample page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import Vue from 'vue';
 import Comopnent from 'vue-class-component';
+import './App.scss';
 
 @Comopnent
 export default class App extends Vue {
   render() {
     return (
       <div id="app">
-        <h1 style={{ textAlign: 'center' }}>
-          Welcome to Your Vue.js + TypeScript App
-        </h1>
+        <div class="landing">
+          <h1>Vue-Aplayer</h1>
+          <h3>🍰 A beautiful HTML5 music player for Vue.js.</h3>
+          <div class="aplayer-wrap" />
+          <div class="landing-buttons">
+            <a
+              class="landing-button"
+              href="https://github.com/MoePlayer/vue-aplayer"
+              target="_blank"
+            >
+              GitHub
+            </a>
+            {/* eslint-disable-next-line no-script-url */}
+            <a class="landing-button" href="javascript:;">
+              Docs
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
