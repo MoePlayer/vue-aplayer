83a22ed75054b0c37f8ce3851501e0f0e6aeb32a
u3u <qwq@qwq.cat>
2018-05-21 11:46:33 +0800

    feat: remove hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import Vue from 'vue';
-import Tsx from 'vue-tsx-support';
 import Comopnent from 'vue-class-component';
-import HelloWorld from 'components/HelloWorld';
-import './App.scss';
 
 @Comopnent
 export default class App extends Vue {
   render() {
     return (
       <div id="app">
-        {/* eslint-disable-next-line global-require */}
-        <img src={require('assets/logo.png')} />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        <h1 style={{ textAlign: 'center' }}>
+          Welcome to Your Vue.js + TypeScript App
+        </h1>
       </div>
     );
   }
