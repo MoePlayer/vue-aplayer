00855c3aa090cc524f6e7a793e0c37814459a9fc
u3u <qwq@qwq.cat>
2018-05-25 19:10:18 +0800

    chore: remove unused module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import App from './App';
-import store from './store';
 
 Vue.config.devtools = true;
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
   render: h => h(App),
 }).$mount('#app');
