0f05b34b97161252705f7f2efedf0f29b41610df
u3u <qwq@qwq.cat>
2018-05-25 16:25:55 +0800

    feat: add main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Inject } from 'vue-property-decorator';
+import Lyric from './Lyric';
+import Controller from './Controller';
+
+@Component
+export default class Main extends Vue {
+  @Inject()
+  private readonly aplayer!: { fixed: boolean; currentMusic: APlayer.Audio };
+
+  private get music() {
+    const { currentMusic } = this.aplayer;
+    return {
+      name: currentMusic.name,
+      artist: currentMusic.artist ? ` - ${currentMusic.artist}` : '',
+    };
+  }
+
+  render() {
+    const { music } = this;
+    const { fixed } = this.aplayer;
+
+    return (
+      <div class="aplayer-info">
+        <div class="aplayer-music">
+          <span class="aplayer-title">{music.name}</span>
+          <span class="aplayer-author">{music.artist}</span>
+        </div>
+        {!fixed ? <Lyric /> : null}
+        {this.$slots.default}
+      </div>
+    );
+  }
+}
