be5cd06cb4598e17f12a56be6a63920cb5720360
u3u <qwq@qwq.cat>
2018-05-27 15:05:24 +0800

    feat(components): reset playback progress after playback completes

--- src/packages/@moefe/vue-aplayer/components/APlayer.tsx
+++ src/packages/@moefe/vue-aplayer/components/APlayer.tsx
@@ -179,6 +179,7 @@ export default class APlayer extends Vue {
 
   @Watch('media.ended')
   private handleChangeEnded() {
+    this.currentPlayed = 0;
     switch (this.loop) {
       default:
       case 'all':
